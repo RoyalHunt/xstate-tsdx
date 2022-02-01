106758fd7d1835cd1d9fc507bcf1e6d44b07bca7
Alexandr Nituleac <nituleacal@gmail.com>
2022-02-01 21:22:24 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useMachine } from "@xstate/react";
+import { createMachine } from "xstate";
+
+type TrafficLightEvent = { type: "NEXT" };
+
+type TraffiLightState =
+  | { value: "green"; context: undefined }
+  | { value: "yellow"; context: undefined }
+  | { value: "red"; context: undefined };
+
+export const trafficLightMachine = createMachine<
+  undefined, // We don’t have context, so we type it as undefined
+  TrafficLightEvent,
+  TraffiLightState
+>({
+  id: "trafficLight",
+  initial: "red",
+  states: {
+    green: {
+      on: { NEXT: "yellow" },
+    },
+    yellow: {
+      on: { NEXT: "red" },
+    },
+    red: {
+      on: { NEXT: "green" },
+    },
+  },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [current, send] = useMachine(trafficLightMachine);
+  console.log("🚀 ~ file: App.tsx ~ line 38 ~ App ~ send", send);
+  console.log("🚀 ~ file: App.tsx ~ line 38 ~ App ~ current", current);
+
+  return <div>fsdf</div>;
 }
 
 export default App;
